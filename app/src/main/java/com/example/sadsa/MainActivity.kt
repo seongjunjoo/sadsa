cf759c7588816b64cd02257357745399df9f923e
smilegon <real_estate_jun@naver.com>
2023-07-02 02:12:22 +0900

    123

--- app/src/main/java/com/example/sadsa/MainActivity.kt
+++ app/src/main/java/com/example/sadsa/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.sadsa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ok_button
+        cancel_button
+
+
     }
 }
