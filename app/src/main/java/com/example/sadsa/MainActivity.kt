7c8475b3330734c03b519b3852691f6af3d9c9bd
smilegon <real_estate_jun@naver.com>
2023-07-02 02:17:52 +0900

    123

--- app/src/main/java/com/example/sadsa/MainActivity.kt
+++ app/src/main/java/com/example/sadsa/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sadsa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ok_button
-        cancel_button
+        ok_button.setOnClickListener {  // 확인용 버튼
+            Log.d("메인화면", "확인 버튼 눌림")
+        }
+        cancel_button.setOnClickListener { // 취소버튼
+            Log.d("메인화면","이전화면 버튼 눌림")
+        }
 
 
     }
