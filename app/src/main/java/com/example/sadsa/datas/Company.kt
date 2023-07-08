4486df9119a3bb9445f5672df74dc2bfb4d2ef41
smilegon <real_estate_jun@naver.com>
2023-07-08 12:58:48 +0900

    123123312

---
+++ app/src/main/java/com/example/sadsa/datas/Company.kt
@@ -0,0 +1,12 @@
+package com.example.sadsa.datas
+
+/// 날짜 , 회사명, 바이어 이름, 핵심사업, 세부사항 등
+
+
+class Company(
+    val date: Int ,
+    val name : String,
+    val buyername :String,
+    val keybusiness :String,
+    val detail :String) {
+}
